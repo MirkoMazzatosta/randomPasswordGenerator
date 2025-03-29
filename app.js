5e77cedba1c858092aad57076718c8779fb6bd20
MazzatostaMirko <mazzatostamirko@gmail.com>
2025-03-29 21:43:12 +0100

    Project Completed

---
+++ app.js
@@ -0,0 +1,57 @@
+// Random Password Generator
+// This script generates a random password based on user-defined criteria
+// and displays it in the console.
+// The password can include lowercase letters, uppercase letters, numbers,
+// and symbols, and the length can be specified by the user.
+
+
+// Function to generate a random password
+
+function generatePassword(length, 
+    includeLowercase, 
+    includeUppercase, 
+    includeNumbers, 
+    includeSymbols) {
+
+        const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
+        const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        const numberChars = '0123456789';
+        const symbolChars = '!@#$%^&*()+?';
+
+        let allowedChars = '';
+        let password = '';
+        allowedChars += includeLowercase ? lowercaseChars : ''; 
+        allowedChars += includeUppercase ? uppercaseChars : '';
+        allowedChars += includeNumbers ? numberChars : '';
+        allowedChars += includeSymbols ? symbolChars : '';
+
+        if (length <= 0) {
+            return 'Password length must be greater than 0.';
+        }
+
+        if (allowedChars.length === 0) {
+            return 'Please select at least one character type.';
+        }
+
+        for (let i = 0; i < length; i++) {
+            const randomIndex = Math.floor(Math.random() * allowedChars.length);
+            password += allowedChars[randomIndex];
+        }
+
+        return password;
+
+    }
+const passwordLength = 12;
+const includeLowercase = true;
+const includeUppercase = true;
+const includeNumbers = true;
+const includeSymbols = true;
+
+const password = generatePassword(passwordLength,
+    includeLowercase,
+    includeUppercase,
+    includeNumbers,
+    includeSymbols);
+
+// Display the generated password in the console
+console.log(`Generated Password: ${password}`);
